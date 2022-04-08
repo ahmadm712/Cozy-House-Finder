e6269608a2090f7c34fc859804adb893b8d44fba
ahmadm712 <ahmadmuji2808@gmail.com>
2022-04-08 08:16:05 +0700

    membuat model city,spac dan tips

---
+++ lib/models/city.dart
@@ -0,0 +1,12 @@
+class CityModel {
+  String id;
+  String name;
+  String imageUrl;
+  bool isPopular;
+  CityModel({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+    required this.isPopular,
+  });
+}
