7697888db04743d78be6ed699a12c9183a376466
ahmadm712 <ahmadmuji2808@gmail.com>
2022-04-08 08:15:44 +0700

    update style

--- lib/common/style.dart
+++ lib/common/style.dart
@@ -6,6 +6,9 @@ Color orangeColor = const Color(0xffFF9376);
 Color blackColor = const Color(0xff000000);
 Color whiteColor = const Color(0xffFFFFFF);
 Color greyColor = const Color(0xff82868E);
+Color cityCardColor = const Color(0xffF6F7F8);
+
+double edge = 24;
 
 TextStyle blackTextStyle = GoogleFonts.poppins(
   fontWeight: FontWeight.w500,
@@ -25,4 +28,9 @@ TextStyle greyTextStyle = GoogleFonts.poppins(
 TextStyle purpleTextStyle = GoogleFonts.poppins(
   fontWeight: FontWeight.w500,
   color: purpleColor,
+);
+
+TextStyle regularTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  color: blackColor,
 );
